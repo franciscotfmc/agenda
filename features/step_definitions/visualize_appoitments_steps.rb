1b172613754d25ae51ccbf533254fa8fa5ff9a1f
Francisco <f.thiene@gmail.com>
2013-05-31 01:28:26 -0300

    Make scenarios of second feature pass

---
+++ features/step_definitions/visualize_appoitments_steps.rb
@@ -0,0 +1,21 @@
+Given(/^I visit the appointments page$/) do
+  visit appointments_path
+end
+
+When(/^I don't have any appointments$/) do
+  Appointment.delete_all
+end
+
+Then(/^I should see a message telling me that$/) do
+  page.should have_selector('h2', text: "No appointments...")
+end
+
+When(/^I have more than (\d+) appointments$/) do |arg1|
+  visit new_appointment_path
+  fill_in "Appointment Description", with: "Appointment 1"
+  click_button "Add"
+end
+
+Then(/^I should see a list of appointments$/) do
+  page.should have_selector('ul')
+end
